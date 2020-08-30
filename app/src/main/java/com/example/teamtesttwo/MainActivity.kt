9a3439fee3f5b0a875631ccaf64658ad5a56f755
Code Kareem <kareemkimo575@gmail.com>
2020-08-30 22:56:18 +0200

    Test Two Master Push Welcome Again

--- app/src/main/java/com/example/teamtesttwo/MainActivity.kt
+++ app/src/main/java/com/example/teamtesttwo/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("HII WELCOME TO MY BRANCH")
+        println("Welcome again")
     }
 }
